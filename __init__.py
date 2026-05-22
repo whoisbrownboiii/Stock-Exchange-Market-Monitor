85809068577fc9caeb8e31d4da158ecb883c600a
Thokozile Mabo <thokozilemabo17@gmail.com>
2026-05-22 11:57:54 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from yahoo_finance import get_live_prices, get_historical_prices, get_sector_etf_return
-from fred_api import get_macro_indicators
-from calculator import run_calculations
-from alert import evaluate_alert, format_alert_message
-from file_writer import write_report
